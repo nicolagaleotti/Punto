e3d6566680fb9e7dc2a8b2469ff709cabeeff69f
Nicola Galeotti <niccio2802@outlook.it>
2020-11-18 11:13:16 +0100

    Add project files.

---
+++ Punto/Punto2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Punto
+{
+    public class Punto2
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Punto2(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Punto/Punto3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Punto
+{
+    public class Punto3 : Punto2
+    {
+        public double Z { get; set; }
+
+        public Punto3(double x, double y, double z) : base(x, y)
+        {
+            Z = z;
+        }
+    }
+}
